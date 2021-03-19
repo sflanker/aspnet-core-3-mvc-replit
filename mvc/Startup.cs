520bdba37ba7fa931bec8e1f2470c8f4eea03d85
Paul Wheeler <sflanker@hotmail.com>
2021-03-19 09:51:59 +0000

    Finally got registration and login working on replit.com

--- mvc/Startup.cs
+++ mvc/Startup.cs
@@ -1,7 +1,9 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,17 +22,36 @@ namespace MvcSample {
 
       services.AddSingleton<IUserStore<IdentityUser>>(MemoryUserStore.Instance);
 
+      EnableReplitIFrameHosting(services);
+
       services.AddControllersWithViews();
       services.AddRazorPages();
+    }
+
+    private static void EnableReplitIFrameHosting(IServiceCollection services) {
+      services.ConfigureApplicationCookie(options => {
+        options.Cookie.SameSite = SameSiteMode.None;
+      });
+
+      services.Configure<ForwardedHeadersOptions>(options => {
+        options.ForwardedHeaders =
+          ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+
+        options.KnownNetworks.Clear();
+        options.KnownProxies.Clear();
+      });
 
       services.AddAntiforgery(options => {
         options.SuppressXFrameOptionsHeader = true;
-        options.Cookie.SameSite = SameSiteMode.Lax;
+        options.Cookie.SameSite = SameSiteMode.None;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
       });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+      app.UseForwardedHeaders();
+
       if (env.IsDevelopment()) {
         app.UseDeveloperExceptionPage();
       } else {
@@ -46,6 +67,21 @@ namespace MvcSample {
 
       app.UseAuthentication();
       app.UseAuthorization();
+
+      /*
+      // HTTP Request Debugging
+      app.Use((context, next) => {
+        Console.Error.WriteLine($"{context.Request.Method} {context.Request.Path} HTTP/1.1");
+        foreach (var header in context.Request.Headers) {
+          foreach (var value in header.Value) {
+            Console.Error.WriteLine($"{header.Key}: {value}");
+          }
+        }
+        Console.Error.WriteLine();
+
+        return next();
+      });
+      */
 
       app.UseEndpoints(endpoints => {
         endpoints.MapControllerRoute(
